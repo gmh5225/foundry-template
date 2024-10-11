eb209c3b69f6a5482316fc968db4e6a37ad6301b
gmh5225 <2315157@qq.com>
2024-10-11 17:21:10 +0800

    bank test

---
+++ src/Bank.sol
@@ -0,0 +1,68 @@
+pragma solidity ^0.8.13;
+
+contract Bank {
+    address public admin;
+    mapping(address => uint256) public balances;
+    address[3] public topDepositors;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    // Receive ETH
+    receive() external payable {
+        // Call deposit function
+        deposit();
+    }
+
+
+    // Deposit function
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit amount must be greater than 0");
+        balances[msg.sender] += msg.value;
+        updateTopDepositors(msg.sender);
+    }
+
+    // Update top 3 depositors
+    function updateTopDepositors(address depositor) private {
+        uint256 depositAmount = balances[depositor];
+        uint256 index = 3;
+        
+        for (uint i = 0; i < 3; i++) {
+            if (depositAmount > balances[topDepositors[i]]) {
+                index = i;
+                break;
+            }
+        }
+        
+        if (index < 3) {
+            for (uint i = 2; i > index; i--) {
+                topDepositors[i] = topDepositors[i-1];
+            }
+            topDepositors[index] = depositor;
+        }
+    }
+
+    // Withdrawal function (only callable by admin)
+    function withdraw(uint256 amount) external {
+        require(msg.sender == admin, "Only admin can withdraw");
+        require(amount <= address(this).balance, "Insufficient balance");
+        payable(admin).transfer(amount);
+    }
+
+    // Query contract balance
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    // Query top 3 depositors
+    function getTopDepositors() public view returns (address[3] memory) {
+        return topDepositors;
+    }
+
+    // Function to get deposit amount for a specific depositor
+    function getDepositAmount(address depositor) public view returns (uint256) {
+        return balances[depositor];
+    }
+}
+

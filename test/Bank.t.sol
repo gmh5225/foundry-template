bb283fe95e32dfa521d76441f5033fd56df69118
gmh <13917777+gmh5225@users.noreply.github.com>
2024-10-11 17:32:48 +0800

    Update Bank.t.sol

--- test/Bank.t.sol
+++ test/Bank.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import { Test } from "forge-std/src/Test.sol";
-import { Bank } from "../src/Bank.sol";
+import "forge-std/Test.sol";
+import "../src/Bank.sol";
 
 contract BankTest is Test {
     Bank public bank;

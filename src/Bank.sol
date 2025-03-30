86a16f1d0c5c8a3806dd8535ff63798b869a94ba
gmh5225.eth <13917777+gmh5225@users.noreply.github.com>
2025-03-31 05:10:29 +0800

    Update Bank.sol

--- src/Bank.sol
+++ src/Bank.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.29;
 
 import { Address } from "@openzeppelin/contracts/utils/Address.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
